c9d815492588e4308c11dc5b8af1f1688b1b3e6e
손근양 <ky.son@kt.com>
2020-01-17 15:22:01 +0900

    프로젝트명을 hot-deals에서 hotdeals로 변경

--- hotdeals-broker-managerement/src/main/java/com/kt/Application.java
+++ hotdeals-broker-managerement/src/main/java/com/kt/Application.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -39,21 +38,6 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		log.info("Application start...");
 		SpringApplication.run(Application.class, args);
-	}
-
-	/**
-	 * Configure the application. Normally all you would need to do is to add sources
-	 * (e.g. config classes) because other settings have sensible defaults. You might
-	 * choose (for instance) to add default command line arguments, or set an active
-	 * Spring profile.
-	 *
-	 * @param application a builder for the application context
-	 * @return the spring application builder
-	 * @see SpringApplicationBuilder
-	 */
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
 	}
 
 }
