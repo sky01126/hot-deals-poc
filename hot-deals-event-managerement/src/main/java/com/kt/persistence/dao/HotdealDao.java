763c2193b75a05c3173d99ef051d0bb706155c6c
손근양 <ky.son@kt.com>
2020-01-17 12:57:14 +0900

    Hot Deals의 네이밍에 맞춰서 이름 변경

---
+++ hot-deals-event-managerement/src/main/java/com/kt/persistence/dao/HotdealDao.java
@@ -0,0 +1,24 @@
+package com.kt.persistence.dao;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.stereotype.Repository;
+
+import com.kt.commons.dto.request.HotdealRequest;
+
+@Repository
+public class HotdealDao {
+
+	@Resource(name = "stringRedisTemplate")
+	private HashOperations<String, String, String> hashOperations;
+
+	public String getEventFcfs(String eventId, String phoneNo) {
+		return hashOperations.get(eventId, phoneNo);
+	}
+
+	public boolean putEventFcfs(HotdealRequest params) {
+		return hashOperations.putIfAbsent(params.getEventId(), params.getPhoneNo(), params.getName());
+	}
+
+}
--- hot-deals-poc-process-manager-webapp/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mvn dockerfile:build
-docker images
